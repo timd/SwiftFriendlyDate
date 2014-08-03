6dc9221116ab0ab6d4bb59758618988fa7b64b07
Tim Duckett <tim@timduckett.co.uk>
2014-08-03 18:14:46 +0200

    Finished tests for full date function
    - WIP: amend extension & tests to return nil if invalid dates passed in
    - build tests/function for partial dates
    - Updated README

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 SwiftFriendlyDate
 =================
 
-Being a first try at creating a Swift extension, to add a friendly date formatter to the NSDate class
+Being a first try at creating a Swift extension, to add a friendly date formatter to the NSDate class that returns time intervals between two dates, for example:
+
+Being a small category on `NSDate` to return an `NSString` containing friendly time intervals between two dates, for example:
+
+* `30 secs`
+* `2 hours 22 secs`
+* `1 day 10 hours 15 secs`
+
+It extends the `NSDate` class to add two methods:
+
+* `func friendlyIntervalBetweenDates(firstDate: NSDate, secondDate: NSDate) -> String`
+* `func partialIntervalBetweenDates(firstDate: NSDate, secondDate:NSDate) -> String`
+
+`friendlyIntervalBetweenDates(firstDate: NSDate, secondDate: NSDate)` will return a full days, hours, mins and seconds string.
+
+`partialIntervalBetweenDates(firstDate: NSDate, secondDate:NSDate)` will ignore seconds if the time difference is more than one minute, and ignore minutes if the difference is more than one hour - e.g.
+
+* a difference of 1 min 20 secs will `1 min`
+* a difference of 1 hour 20 mins 30 seconds will return `1 hour`
+* a difference of 1 day 3 hours 40 mins 22 seconds will return `1 day 3 hours`
+
+Both methods handle pluralisation.
+
+Work in progress
+---
+* `friendlyIntervalBetweenDates(firstDate: NSDate, secondDate: NSDate)` will eventually return an optional nil value if invalid dates are passed in (e.g. the second date is before the first date)
+* `partialIntervalBetweenDates(firstDate: NSDate, secondDate:NSDate)` is not yet implemented
+
+Usage
+---
+
+Include 'FriendlyDate.swift` in your project.
+
+    var firstDate = NSDate()
+    var secondDate = NSDate(timeInterval: (90), sinceDate: firstDate)
+    let response = NSDate().friendlyIntervalBetweenDates(firstDate, secondDate: secondDate)
+
+will return
+
+`1 min 30 secs`
+
+Unit tests
+---
+
+`SwiftFriendlyDateTests` and `SwiftFriendlyPartialDateTests` are XCTest unit tests.
--- SwiftFriendlyDateTests/SwiftFriendlyDateTests.swift
+++ SwiftFriendlyDateTests/SwiftFriendlyDateTests.swift
@@ -69,7 +69,6 @@ class SwiftFriendlyDateTests: XCTestCase {
     }
 
     // should return 2 minutes if the two dates are 120 secs apart
-    
     func testTwoMinutes() {
         var firstDate = NSDate()
         var secondDate = NSDate(timeInterval: 120, sinceDate: firstDate)
@@ -78,7 +77,6 @@ class SwiftFriendlyDateTests: XCTestCase {
     }
     
     // should return 2 mins 30 secs if the two dates are 2 mins and 30 secs apart", ^{
-
     func testTwoMinutes30Seconds() {
         var firstDate = NSDate()
         var secondDate = NSDate(timeInterval: 150, sinceDate: firstDate)
@@ -86,20 +84,69 @@ class SwiftFriendlyDateTests: XCTestCase {
         XCTAssertTrue(response == "2 mins 30 secs", "should return 2 mins 30 secs, but returned '\(response)'")
     }
     
-    // it(@"should return 30 minutes if the two dates are 1800 of each other", ^{
-//    
-//    it(@"should return 31 mins if the two dates are 1889 of each other", ^{
-//    
-//    it(@"should return 1 hour if the two dates are 3600 secs apart", ^{
-//    
-//    it(@"should return 1 hour if the two dates are 5400 seconds of each other", ^{
-//    
-//    it(@"should return 1 day if the two dates are 86,400 secs apart", ^{
-//    
-//    it(@"should return 1 day 12 hours if the two dates are 129600 secs apart", ^{
-//    
-//    it(@"should return 2 days if the two dates are 172800 secs apart", ^{
-//    
-//    it(@"should return 3 days 4 hours if the two dates are 274053 apart", ^{
+    // should return 30 minutes if the two dates are 1800 of each other
+    func test30Minutes() {
+        var firstDate = NSDate()
+        var secondDate = NSDate(timeInterval: (30 * 60), sinceDate: firstDate)
+        let response = NSDate().friendlyIntervalBetweenDates(firstDate, secondDate: secondDate)
+        XCTAssertTrue(response == "30 mins", "should return 30 mins, but returned '\(response)'")
+    }
+    
+    // should return 31 mins if the two dates are 1889 of each other
+    func test31Minutes() {
+        var firstDate = NSDate()
+        var secondDate = NSDate(timeInterval: (31*60), sinceDate: firstDate)
+        let response = NSDate().friendlyIntervalBetweenDates(firstDate, secondDate: secondDate)
+        XCTAssertTrue(response == "31 mins", "should return 31 mins, but returned '\(response)'")
+    }
+
+    // should return 1 hour if the two dates are 3600 secs apart
+    func test1Hour() {
+        var firstDate = NSDate()
+        var secondDate = NSDate(timeInterval: (60*60), sinceDate: firstDate)
+        let response = NSDate().friendlyIntervalBetweenDates(firstDate, secondDate: secondDate)
+        XCTAssertTrue(response == "1 hour", "should return 1 hour, but returned '\(response)'")
+    }
+    
+    // should return 1 hour 30 mins if the two dates are 5400 seconds of each other
+    func test1Hour30Minutes() {
+        var firstDate = NSDate()
+        var secondDate = NSDate(timeInterval: (90*60), sinceDate: firstDate)
+        let response = NSDate().friendlyIntervalBetweenDates(firstDate, secondDate: secondDate)
+        XCTAssertTrue(response == "1 hour 30 mins", "should return 1 hour 30 mins, but returned '\(response)'")
+    }
+    
+    // should return 1 day if the two dates are 86,400 secs apart
+    func test1day() {
+        var firstDate = NSDate()
+        var secondDate = NSDate(timeInterval: (24*60*60), sinceDate: firstDate)
+        let response = NSDate().friendlyIntervalBetweenDates(firstDate, secondDate: secondDate)
+        XCTAssertTrue(response == "1 day", "should return 1 day, but returned '\(response)'")
+    }
+    
+    // should return 1 day 12 hours if the two dates are 129600 secs apart
+    func test1day12hours() {
+        var firstDate = NSDate()
+        var secondDate = NSDate(timeInterval: (36*60*60), sinceDate: firstDate)
+        let response = NSDate().friendlyIntervalBetweenDates(firstDate, secondDate: secondDate)
+        XCTAssertTrue(response == "1 day 12 hours", "should return 1 day 12 hours, but returned '\(response)'")
+    }
+    
+    // should return 2 days if the two dates are 172800 secs apart
+    func test2days() {
+        var firstDate = NSDate()
+        var secondDate = NSDate(timeInterval: (48*60*60), sinceDate: firstDate)
+        let response = NSDate().friendlyIntervalBetweenDates(firstDate, secondDate: secondDate)
+        XCTAssertTrue(response == "2 days", "should return 2 days, but returned '\(response)'")
+    }
+    
+    // should return 3 days 4 hours if the two dates are 274053 apart
+    func test3days4hours() {
+        var firstDate = NSDate()
+        var secondDate = NSDate(timeInterval: (76*60*60), sinceDate: firstDate)
+        let response = NSDate().friendlyIntervalBetweenDates(firstDate, secondDate: secondDate)
+        XCTAssertTrue(response == "3 days 4 hours", "should return 3 days 4 hours, but returned '\(response)'")
+    }
+    
     
 }
